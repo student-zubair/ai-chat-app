91f400df152e54271da5726495765215cb989dfb
Mohammed Zubair <zubair9141@gmail.com>
2026-04-02 20:51:27 +0530

    FIX CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const chatRoutes = require("./routes/chat");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
